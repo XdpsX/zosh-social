6b41d5f932c853e0f745be8ceaa8ea4688f5111a
Night <dranhata@gmail.com>
2024-01-19 08:11:17 +0700

    first commit

---
+++ backend/src/main/java/com/zosh/zoshsocial/ZoshSocialApplication.java
@@ -0,0 +1,13 @@
+package com.zosh.zoshsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoshSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoshSocialApplication.class, args);
+	}
+
+}
