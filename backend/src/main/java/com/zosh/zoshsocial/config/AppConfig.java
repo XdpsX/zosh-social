49ab42a7bde5e618058ebc35f9131e735aebe64f
Night <dranhata@gmail.com>
2024-02-22 22:29:38 +0700

    Security config

---
+++ backend/src/main/java/com/zosh/zoshsocial/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.zosh.zoshsocial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+//            .sessionManagement(
+//                    management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            )
+            .authorizeHttpRequests(auth ->
+                auth
+                        .requestMatchers("/api/v1/**").authenticated()
+                        .anyRequest().permitAll()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+                ;
+
+        return http.build();
+    }
+}
